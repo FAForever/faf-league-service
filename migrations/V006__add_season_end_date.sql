59a4dbda7bfce64ba1d3fce976be70b20c615944
BlackYps <jonas-fb@gmx.de>
2021-09-26 23:56:00 +0200

    Fix SQL statement

--- migrations/V006__add_season_end_date.sql
+++ migrations/V006__add_season_end_date.sql
@@ -1,1 +1,3 @@
-UPDATE league_season SET TIMESTAMP("2021-10-31",  "23:59:00") WHERE league_season.id>2;
+SET time_zone='+00:00';
+
+UPDATE league_season SET end_date = "2021-10-31 23:59:00" WHERE league_season.id>2;

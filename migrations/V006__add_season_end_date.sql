a0fa846ede562890ce05c91615788a1ea0e1d321
BlackYps <52536103+BlackYps@users.noreply.github.com>
2021-09-30 18:37:28 +0200

    Make season endDate column not nullable (#35)

--- migrations/V006__add_season_end_date.sql
+++ migrations/V006__add_season_end_date.sql
@@ -1,3 +1,5 @@
 SET time_zone='+00:00';
 
-UPDATE league_season SET end_date = "2021-10-31 23:59:00" WHERE league_season.id>2;
+UPDATE league_season SET end_date = "2021-10-31 23:59:00" WHERE end_date IS NULL;
+
+ALTER TABLE league_season MODIFY COLUMN end_date DATETIME NOT NULL;

b7614c3742f71a546024a22a5d4020e028636366
BlackYps <52536103+BlackYps@users.noreply.github.com>
2021-09-30 11:50:33 +0200

    Select active season by date (#33)

---
+++ migrations/V006__add_season_end_date.sql
@@ -0,0 +1,3 @@
+SET time_zone='+00:00';
+
+UPDATE league_season SET end_date = "2021-10-31 23:59:00" WHERE league_season.id>2;
--- service/league_service/league_service.py
+++ service/league_service/league_service.py
@@ -1,10 +1,11 @@
 import asyncio
 from collections import defaultdict
+from datetime import datetime
 from typing import Dict
 
 import aiocron
 from aio_pika import IncomingMessage
-from sqlalchemy import and_, select
+from sqlalchemy import and_, between, select
 from sqlalchemy.dialects.mysql import insert
 
 from service import config
@@ -67,7 +68,7 @@ class LeagueService:
                     .outerjoin(league)
                     .outerjoin(leaderboard)
                 )
-                .where(league_season.c.end_date == None)
+                .where(between(datetime.now(), league_season.c.start_date, league_season.c.end_date))
             )
             result = await conn.execute(sql)
             division_rows = await result.fetchall()

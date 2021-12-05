49b5941c3cbe67ed65cde88ea3b2a0bbc4bbd0c1
BlackYps <jonas-fb@gmx.de>
2021-12-05 14:17:30 +0100

    Change season name keys

--- migrations/V011__drop_test_data_and_add_official_seasons.sql
+++ migrations/V011__drop_test_data_and_add_official_seasons.sql
@@ -20,10 +20,10 @@ INSERT INTO league (id, technical_name, name_key, description_key, image_url, me
 
 
 INSERT INTO league_season (id, league_id, leaderboard_id, placement_games, season_number, name_key, start_date, end_date) VALUES
-  (1, 1, 2, 10, 1, "1v1_league.season.1", "2021-12-15 00:00:00", "2022-03-31 23:59:00"),
-  (2, 2, 3, 10, 1, "2v2_league.season.1", "2021-12-15 00:00:00", "2022-03-31 23:59:00"),
-  (3, 3, 4, 10, 1, "4v4_fs_league.season.1", "2021-12-15 00:00:00", "2022-03-31 23:59:00"),
-  (4, 4, 5, 10, 1, "4v4_sud_league.season.1", "2021-12-15 00:00:00", "2022-03-31 23:59:00");
+  (1, 1, 2, 10, 1, "1v1_season", "2021-12-15 00:00:00", "2022-03-31 23:59:00"),
+  (2, 2, 3, 10, 1, "2v2_season", "2021-12-15 00:00:00", "2022-03-31 23:59:00"),
+  (3, 3, 4, 10, 1, "4v4_fs_season", "2021-12-15 00:00:00", "2022-03-31 23:59:00"),
+  (4, 4, 5, 10, 1, "4v4_sud_season", "2021-12-15 00:00:00", "2022-03-31 23:59:00");
 
 INSERT INTO league_season_division (id, league_season_id, division_index, description_key, name_key) VALUES
   ( 1, 1, 1, "1v1_season_1.division.1", "bronze"),

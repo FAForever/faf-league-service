57c1b22785d04c3c43a88288c519cb2261478c8b
BlackYps <jonas-fb@gmx.de>
2021-12-05 14:16:00 +0100

    Change season name keys

--- migrations/V010__add_season_number_column.sql
+++ migrations/V010__add_season_number_column.sql
@@ -1,7 +1,7 @@
 ALTER table league_season
 ADD COLUMN `season_number` INTEGER NOT NULL AFTER placement_games;
 
-UPDATE league_season SET season_number = 1 WHERE id = 1;
-UPDATE league_season SET season_number = 1 WHERE id = 2;
-UPDATE league_season SET season_number = 2 WHERE id = 3;
-UPDATE league_season SET season_number = 2 WHERE id = 4;
+UPDATE league_season SET season_number = 1, name_key = "2v2_season" WHERE id = 1;
+UPDATE league_season SET season_number = 1, name_key = "1v1_season"  WHERE id = 2;
+UPDATE league_season SET season_number = 2, name_key = "2v2_season"  WHERE id = 3;
+UPDATE league_season SET season_number = 2, name_key = "1v1_season"  WHERE id = 4;

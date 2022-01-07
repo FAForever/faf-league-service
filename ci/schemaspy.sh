4bd7ce9a4eceb680bcdf8d5dd84132d3544b28da
BlackYps <jonas-fb@gmx.de>
2022-01-07 16:54:11 +0100

    Publish schemaspy to github pages

--- ci/schemaspy.sh
+++ ci/schemaspy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-echo 'travis_fold:start:schemaspy'
-
 echo '# Generating db diagram with Schemaspy'
 
 SCHEMASPY_OUTPUT_DIR=$PWD/public
@@ -12,10 +10,8 @@ cat "$PWD/ci/schemaspy.properties"
 mkdir -p "${SCHEMASPY_OUTPUT_DIR}"
 chmod a+rw "${SCHEMASPY_OUTPUT_DIR}"
 
-docker run --network="faf" \
+docker run --network="host" \
             -v "$PWD/ci/schemaspy.properties:/schemaspy.properties" \
             -v "${SCHEMASPY_OUTPUT_DIR}:/output" \
             schemaspy/schemaspy:latest \
            || { echo "Could not generate schema"; exit 1; }
-
-echo 'travis_fold:end:schemaspy'

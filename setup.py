06baf67ac1dd05f50f50226c78f2c1989c696e3e
BlackYps <jonas-fb@gmx.de>
2023-05-01 18:56:14 +0200

    Update package info

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import find_packages
 import service
 
 setup(
-    name="Forged Alliance Forever Server",
-    version="1.0",
+    name="Forged Alliance Forever League Service",
+    version="1.3.0",
     packages=["service"] + find_packages(),
     url="http://www.faforever.com",
     license=service.__license__,
     author=service.__author__,
     author_email=service.__contact__,
-    description="Trueskill rating service",
+    description="Service to provide leaderboards for Forged Alliance Forever",
     include_package_data=True,
 )

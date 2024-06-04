fbfcd2a414c077d95cb3f344f5bfbeca71aaf62c
BlackYps <jonas-fb@gmx.de>
2024-06-05 00:01:11 +0200

    Refactor database wrapper to use sqlalchemy engine instead of aiomysql

--- service/db/__init__.py
+++ service/db/__init__.py
@@ -1,42 +1,27 @@
-from aiomysql.sa import create_engine
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncEngine
 
 
 class FAFDatabase:
-    def __init__(self, loop):
-        self._loop = loop
-        self.engine = None
-
-    async def connect(
+    def __init__(
         self,
-        host="localhost",
-        port=3306,
-        user="root",
-        password="",
-        db="faf_test",
-        minsize=1,
-        maxsize=1,
+        host: str = "localhost",
+        port: int = 3306,
+        user: str = "root",
+        password: str = "",
+        db: str = "faf_test",
+        **kwargs
     ):
-        if self.engine is not None:
-            raise ValueError("DB is already connected!")
-        self.engine = await create_engine(
-            host=host,
-            port=port,
-            user=user,
-            password=password,
-            db=db,
-            autocommit=True,
-            loop=self._loop,
-            minsize=minsize,
-            maxsize=maxsize,
+        kwargs["future"] = True
+        sync_engine = create_engine(
+            f"mysql+aiomysql://{user}:{password}@{host}:{port}/{db}",
+            **kwargs
         )
 
+        self.engine = AsyncEngine(sync_engine)
+
     def acquire(self):
-        return self.engine.acquire()
+        return self.engine.begin()
 
     async def close(self):
-        if self.engine is None:
-            return
-
-        self.engine.close()
-        await self.engine.wait_closed()
-        self.engine = None
+        await self.engine.dispose()
